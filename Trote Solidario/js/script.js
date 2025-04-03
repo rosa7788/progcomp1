e56737cf55ed4d8026698d79d6bb3c76c9b53611
rosa7788 <rosa05meneghetti@gmail.com>
2025-04-02 22:07:47 -0300

    trote

--- Trote Solidario/js/script.js
+++ Trote Solidario/js/script.js
@@ -2,6 +2,7 @@ function calcular(){
     //recuperar valor da ação social digitado
     //declaração variavel sem tipo
     soma = 0
+    let pontosKit = 0
    let acaoSocial = document.getElementById("acaoSocial").value;
    soma = soma + Number(acaoSocial)
    //recupera o valor da homenagem digitado
@@ -17,27 +18,22 @@ function calcular(){
    let equipe = document.getElementById("equipe").value
    //recupera a cor da equipe
    if (equipe == "Laranja") {
-    let pontoKit = 0
     //equipe é laranja
-        if ((kit) >=97) { 
-            pontosKit = 5000 + ((kit-97)*30)
+        if ((kit) >=97 && suplemento >=49) { 
+            pontosKit = 5000 + ((kit-97)*30) + ((suplemento - 25)*15)
         }
-        else if ((kit) >=78) {
-            pontosKit = 4000 + ((kit-78)*30)
+        else if ((kit) >=78 && suplemento >=39) {
+            pontosKit = 4000 + ((kit-78)*30) + ((suplemento - 25)*15)
         }
-        else if ((kit) >=49) {
-            pontosKit = 2500 + ((kit-49)*30)
+        else if ((kit) >=49 && suplemento >=25) {
+            pontosKit = 2500 + ((kit-49)*30) + ((suplemento - 25)*15)
         }
-        else if ((kit) >=19) {
-            pontosKit = 1000 + ((kit-19)*30)
+        else if ((kit) >=19 && suplemento >=10) {
+            pontosKit = 1000 + ((kit-19)*30) + ((suplemento - 25)*15)
         }
-        else {
-            pontosKit = 0
-        }
-
+        else pontosKit = 0
     }
-   
-
+    alert (pontosKit)
    let suplemento= document.getElementById("suplemento").value;
    soma = soma + Number(30 * suplemento)
    //
